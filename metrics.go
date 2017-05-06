113e059ebf8c73652a8d920148790758926c0829
Lee Keitel <lee@keitel.xyz>
2017-05-06 13:26:49 -0500

    Update import paths

--- metrics.go
+++ metrics.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/lfkeitel/spartan/config"
-	"github.com/lfkeitel/spartan/event"
-	"github.com/lfkeitel/spartan/filters"
-	"github.com/lfkeitel/spartan/metrics"
-	"github.com/lfkeitel/spartan/utils"
+	"github.com/spartanlogs/spartan/config"
+	"github.com/spartanlogs/spartan/event"
+	"github.com/spartanlogs/spartan/filters"
+	"github.com/spartanlogs/spartan/metrics"
+	"github.com/spartanlogs/spartan/utils"
 )
 
 func init() {
